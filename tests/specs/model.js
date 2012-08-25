2444884fd45d92bd4546e37c3dd845902882c9e8
Simone Lusenti <lusenti.s@gmail.com>
2012-08-25 18:49:00 +0200

    Adding specs

--- tests/specs/model.js
+++ tests/specs/model.js
@@ -3,30 +3,30 @@
 
 window.specs.push(function(danilo){
 
-  describe('danilo', function() {
+  describe('danilo', function(){
     var User
       , pippo
       , pluto;
     
     // Defining a User model
-    User = (function() {
-      var User = function() {
+    User = (function(){
+      var User = function(){
         return User.__super__.constructor.apply(this, arguments);
       }
       __extends(User, danilo.Model);
 
       User.prototype.url = '/user/';
       User.prototype.autoValidate = true;
-      User.prototype.attrs = {
+      User.prototype.attrs ={
         password: 'password',
         lengthOfMyUsername: -1,
-        username: {
+        username:{
           defaultValue: 'Default username',
           reactive: true,
-          validators: {
+          validators:{
             minLength: 5,
             maxLength: 100,
-            firstLetterIsUppercase: function(attributeValue) {
+            firstLetterIsUppercase: function(attributeValue){
               return attributeValue[0].toUpperCase() === attributeValue[0];
             }
           }
@@ -40,39 +40,51 @@ window.specs.push(function(danilo){
       username: 'Plutone'
     });
     
-    beforeEach(function() {
+    beforeEach(function(){
       spyOn(window, "alert").andCallFake(console.log);
     });
     
-    it('should export a danilo.Model object', function() {
+    it('should export a danilo.Model object', function(){
       expect(danilo.Model).to.be.a("function");
     });
     
     describe('User', function(){
-      it('should define have some methods', function() {
+      it('should define have some methods', function(){
         expect(User).to.be.a("function");
         expect(pippo.get).to.be.a("function");
         expect(pippo.set).to.be.a("function");
       });
       
-      it('should have a default value as username', function() {
+      it('should have a default value as username', function(){
         expect(pippo.get('username')).to.be(User.prototype.attrs.username.defaultValue);
       });
       
-      it('should have a default value as password', function() {
+      it('should have a default value as password', function(){
         expect(pippo.get('password')).to.be(User.prototype.attrs.password);
       });
       
       describe('#set', function(){
-        it('should set pippo username', function() {
+        it('should set pippo username', function(){
           pippo.set('username', 'Piatto');
           expect(pippo.get('username')).to.be('Piatto');
         });
         
-        it('should leave unchanged other usernames', function() {
+        it('should leave unchanged other usernames', function(){
           expect(pluto.get('username')).to.be('Plutone');
         });
         
+        describe('with validation', function(){
+          var set_result = pippo.set('username', 'lowercase_is_bad');
+          
+          it('should return false if a validator is violated', function(){
+            expect(set_result).to.be(false);
+          });
+          
+          it('should not change value if a validator is violated', function(){
+            expect(pippo.get('username')).to.be('Piatto');
+          });
+          
+        });
       });
       
       
