9bf49e946fa02f887253f6d7863d57ed7cc8a697
Simone Lusenti <lusenti.s@gmail.com>
2012-08-26 16:11:27 +0200

    Travis test 5 - increased maxWait to 20000

--- tests/tests.js
+++ tests/tests.js
@@ -3,8 +3,8 @@
 var expect = require('expect.js')
   , Browser = require('zombie')
   , browser = new Browser({
-        debug: true,
-        maxWait: 20
+        //debug: true,
+        maxWait: 20000
     });
 
 
@@ -14,6 +14,7 @@ describe('navigator', function() {
         browser.visit("http://localhost:8000/tests/specRunner.html", function(e, b){
             
             expect(browser.success).to.be(true);
+            expect(browser.window.danilo).to.be.an("object");
             
             console.log("Browser errors:", browser.errors);
             
