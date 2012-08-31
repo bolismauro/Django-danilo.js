2f1907b73e9a24a9c37a89bfcd5ea06d6c82c3d4
Mauro Bolis <bolismauro@gmail.com>
2012-08-31 19:21:15 +0200

    fixed danilo circular dependence

--- src/view.js
+++ src/view.js
@@ -1,11 +1,11 @@
 "use strict";
 (function() {
 
-  define(['danilo', 'handlebars'], function(danilo, handlebars) {
+  define(['handlebars'], function(handlebars) {
     var View;
-    
+  
     return View = (function() {
-      console.log(danilo);
+      
       function View(viewUrl) {
         // TODO: add a template param?
         View.prototype.url = viewUrl;
@@ -30,10 +30,10 @@
         }
         require(["text!"+template], function(template){
           var rendered_html = Handlebars.compile(template)(ctx);
-          console.log(rendered_html);
           var destination = document.querySelector(selector);
           destination.innerHTML = rendered_html;
-          //danilo.update();
+          //circular dependence..how fix this?
+          require('./danilo').update();
           // maybe use a combination of event+operation is better?
           callback();
         });
