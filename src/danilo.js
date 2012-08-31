4613a31d063bc24d655abfa9e044b0a11846ac2f
Simone Lusenti <lusenti.s@gmail.com>
2012-08-31 10:13:34 +0200

    Added danilo.update() with code from the old router.js file Changed danilo.init()

--- src/danilo.js
+++ src/danilo.js
@@ -4,7 +4,7 @@
   define(['./router', './model', './operation', './remote', './storage', 'handlebars', './view'], function(router, Model, Operation, Remote, storage, Handlebars, View) {
     var render_template
       , danilo
-      , init;
+      , dany;
 
     render_template = function(template, ctx) {
       if (ctx == null) {
@@ -12,86 +12,99 @@
       }
       return Handlebars.compile(template)(ctx);
     };
-
-    init = (function(router) {
+    
+    dany = (function(router) {
+      // Init and update handlers
+      
+      var _formsubmit_factory
+        , _formchange_add_handlers
+        , _fixForms
+        , update
+        , init;
       
       _formsubmit_factory = function(form_tag) {
-        var _this = this;
         return function(e) {
-          var attrs, item, _i, _len, _ref;
-          attrs = {};
-          _ref = form_tag.getElementsByTagName('input');
-          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-            item = _ref[_i];
+          var attrs = {}
+            , inputs;
+          
+          inputs = form_tag.getElementsByTagName('input');
+          inputs = Array.prototype.slice.apply(inputs); // Convert NodeList to Array
+          
+          inputs.forEach(function(item, k){
             if (item.getAttribute('data-skip') !== 'true') {
-              if (!item.getAttribute('name')) {
-                if (typeof console !== "undefined" && console !== null) {
+              if (item.getAttribute('name') == null) {
+                if (console != null) {
                   console.log('[debug-router] this field does not have a name', item);
                 }
               } else {
                 attrs[item.getAttribute('name')] = item.value;
               }
             }
-          }
-          PubSub.publish("submit " + (form_tag.getAttribute('data-bind')), {
+          });
+          
+          PubSub.publish("submit " + form_tag.getAttribute('data-bind'), {
             form: form_tag,
             attrs: attrs
           });
+          
           e.preventDefault();
           return false;
         };
       };
       
       _formchange_add_handlers = function(form_tag) {
-        var item, _i, _len, _ref;
-        _ref = form_tag.getElementsByTagName('input');
-        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-          item = _ref[_i];
+        var inputs;
+        
+        inputs = form_tag.getElementsByTagName('input');
+        inputs = Array.prototype.slice.apply(inputs);
+        
+        inputs.forEach(function(item, k){
           if (item.getAttribute('data-skip') !== 'true') {
-            if (item.getAttribute('name')) {
-              item.onchange = (function(item) {
-                return function() {
-                  return PubSub.publish("change " + (form_tag.getAttribute('data-bind')), {
-                    form: form_tag,
-                    input: item
-                  });
-                };
-              })(item);
+            if (item.getAttribute('name') != null) { 
+              return function() {
+                return PubSub.publish("change " + form_tag.getAttribute('data-bind'), {
+                  form: form_tag,
+                  input: this
+                });
+              };
             }
           }
-        }
+        }); 
       };
-      _fixDOM = function() {
-        var item, real_href, _i, _j, _len, _len1, _ref, _ref1, _results;
-        _ref1 = document.querySelectorAll('form[data-bind]');
-        _results = [];
-        for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
-          item = _ref1[_j];
+      
+      _fixForms = function() {
+        var items;
+        
+        items = document.querySelectorAll('form[data-bind]');
+        items = Array.prototype.slice.apply(items);
+        
+        items.forEach(function(item, k) {
           item.onsubmit = _formsubmit_factory(item);
-          _results.push(_formchange_add_handlers(item));
-        }
-        return _results;
+          _formchange_add_handlers(item);
+        });
       };
+      
+      update = function() {
+        // Call this when you add or remove views
+        _fixForms();
+      }
       
       init = function() {
-        var old_handler,
-          _this = this;
-        old_handler = window.onhashchange;
-        window.onhashchange = function() {
-          _fixDOM();
-          return typeof old_handler === "function" ? old_handler() : void 0;
-        };
-        return _fixDOM();
+        update();
       };
       
-      return init;
+      return {
+        init: init,
+        update: update
+      };
       
     })(router);
 
     exports.danilo = danilo = {
       render_template: render_template,
       router: router,
-      init: init,
+      init: dany.init,
+      update: dany.update,
       Model: Model,
       Operation: Operation,
       storage: storage,
