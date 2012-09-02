8449f6b93571e0c782c322bc78e3af695562b485
Simone Lusenti <lusenti.s@gmail.com>
2012-09-02 13:38:35 +0200

    Restyling

--- src/router.js
+++ src/router.js
@@ -1,35 +1,38 @@
 "use strict";
-(function(){
+
+(function(window){
     // The Router of danilo.js
     define([], function() {
         var router = (function(){
             var router
               , views = {}
               , History = window.History
-              , actualView = undefined;
+              , currentView = undefined;
 
             router = {}
             router.init = function(name) {
                 if (typeof History !== "undefined" && History.enabled) {
                     History.Adapter.bind(window,'statechange',function(){ 
+                        var State = History.getState()
+                          , view_to_load = views[State.hash];
 
-
-                        var State = History.getState(); 
-                        
-                        var view_to_load = views[State.hash];
-
-                        if(view_to_load === undefined){
+                        if("undefined" == typeof view_to_load){
+                            // Usiamo questo pattern:
+                            // undefined e null sono uguali tra loro e sono diversi da qualunque altro valore
+                            // quindi confrontando con == otteniamo true se e solo se il valore è null o undefined.
+                            // L'alternativa lunga è view_to_load === undefined || view_to_load === null
+                            // oppure, in modo più pulito, usando typeof per controllare undefined.
                             console.log("[debug] view not found");
                             return;
+                        } else {
+
+                            if(currentView != null){
+                                currentView.unload();
+                            }
+
+                            currentView = view_to_load;
+                            view_to_load.load();
                         }
-
-                        if(actualView !== undefined ){
-                            actualView.unload();
-                        }
-
-                        actualView = view_to_load;
-
-                        view_to_load.load();
                     });
                 } else {
                     console.log("Danilo.js Routing is not supported by this browser");
@@ -55,6 +58,7 @@
             
             return router;
         })();
+        
         return router;
     });
-}).call(this);
+})(window);
